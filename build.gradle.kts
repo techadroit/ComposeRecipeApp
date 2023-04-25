507ec64c1b15269cdf4baebc07e921fcfd446298
dev.thrivent <devasahayam.nadar@thrivent.com>
2023-04-25 22:53:21 +0530

    code refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,4 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.hilt.dagger) apply false
-    id("com.android.library") version "7.4.0" apply false
 }
