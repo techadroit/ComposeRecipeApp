8ef17ca448a5f997591ba4e75d253ea98c85fb62
dev <denadar@deloitte.com>
2023-07-05 12:01:26 +0530

    code refactoring

--- state_manager/src/main/java/com/state_manager/extensions/ManagerExtensions.kt
+++ state_manager/src/main/java/com/state_manager/extensions/ManagerExtensions.kt
@@ -6,14 +6,6 @@ import com.state_manager.side_effects.SideEffect
 import com.state_manager.state.AppState
 import com.state_manager.test.TestContainer
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
-import kotlinx.coroutines.test.runCurrent
-import kotlinx.coroutines.test.runTest
 
 suspend fun <S : AppState, E : AppEvent, SIDE_EFFECT : SideEffect> Manager<S, E, SIDE_EFFECT>.runCreate(
     initialState: S,
