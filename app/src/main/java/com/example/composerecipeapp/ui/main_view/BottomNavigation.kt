d4851c80ed67485200f2ab9e93da3fa83c0141be
dev <denadar@deloitte.com>
2023-07-27 13:28:15 +0530

    Add navigation rail as a menu to the tablets

--- app/src/main/java/com/example/composerecipeapp/ui/main_view/BottomNavigation.kt
+++ app/src/main/java/com/example/composerecipeapp/ui/main_view/BottomNavigation.kt
@@ -2,28 +2,43 @@ package com.example.composerecipeapp.ui.main_view
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.NavigationRailItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.core.navigtion.navigator.AppNavHost
 import com.core.navigtion.navigator.NavComposable
 import com.feature.home.ui.HomeScreen
-import com.feature.recipe.list.ui.SearchView
 import com.feature.recipe.list.ui.RecipeListScreen
+import com.feature.recipe.list.ui.SearchView
 import com.feature.recipe.video.ui.RecipesVideoList
 import com.feature.saved.recipes.ui.FavouriteRecipeScreen
 import com.feature.settings.ui.SettingsView
-import com.recipe.app.navigation.intent.*
+import com.recipe.app.navigation.intent.HomeViewIntent
+import com.recipe.app.navigation.intent.RecipeListIntent
+import com.recipe.app.navigation.intent.RecipeVideoListIntent
+import com.recipe.app.navigation.intent.SavedRecipeIntent
+import com.recipe.app.navigation.intent.SearchViewIntent
+import com.recipe.app.navigation.intent.SettingsViewIntent
 import com.recipe.app.navigation.provider.MainViewNavigator
 
 @Composable
@@ -35,15 +50,18 @@ fun BottomBar(items: List<BottomBarItems>) {
     }
 
     NavigationBar(
-        modifier = Modifier.height(48.dp),
+        modifier = Modifier
+            .height(48.dp)
+            .fillMaxWidth(),
         containerColor = MaterialTheme.colorScheme.surface,
     ) {
         items.forEachIndexed { index, mainScreen ->
             val isSelected = selectedIndex.value == index
             NavigationBarItem(
                 colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
-                        indicatorColor = MaterialTheme.colorScheme.surface ),
+                    selectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                    indicatorColor = MaterialTheme.colorScheme.surface
+                ),
                 alwaysShowLabel = false,
                 icon = {
                     Icon(
@@ -56,18 +74,61 @@ fun BottomBar(items: List<BottomBarItems>) {
                     selectedIndex.value = index
                     val route = mainScreen.routeName
                     val navController = mainViewNavigator.getNavController()
-                    navController.navigate(route) {
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) {
-                                saveState = true
-                            }
-                        }
-                        restoreState = true
-                        launchSingleTop = true
-                    }
+                    routeTo(route, navController)
                 }
             )
         }
+    }
+}
+
+
+@Composable
+fun NavRail(items: List<BottomBarItems>) {
+    val mainViewNavigator = MainViewNavigator.current
+    val selectedIndex = rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationRail(
+        modifier = Modifier.fillMaxHeight(),
+        containerColor = MaterialTheme.colorScheme.surface,
+    ) {
+        items.forEachIndexed { index, bottomBarItems ->
+            val isSelected = selectedIndex.value == index
+            NavigationRailItem(
+                colors = NavigationRailItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                    indicatorColor = MaterialTheme.colorScheme.surface
+                ),
+
+//                label = { bottomBarItems.tabName },
+                selected = isSelected,
+                icon =  {
+                    Icon(
+                        bottomBarItems.getIcon(index),
+                        contentDescription = bottomBarItems.tabName
+                    )
+                },
+                onClick = {
+                    selectedIndex.value = index
+                    val route = bottomBarItems.routeName
+                    val navController = mainViewNavigator.getNavController()
+                    routeTo(route, navController)
+                }
+            )
+        }
+    }
+}
+
+fun routeTo(route: String, navController: NavController) {
+    navController.navigate(route) {
+        navController.graph.startDestinationRoute?.let { route ->
+            popUpTo(route) {
+                saveState = true
+            }
+        }
+        restoreState = true
+        launchSingleTop = true
     }
 }
 
--- app/src/main/java/com/example/composerecipeapp/ui/main_view/MainScreen.kt
+++ app/src/main/java/com/example/composerecipeapp/ui/main_view/MainScreen.kt
@@ -2,25 +2,32 @@ package com.example.composerecipeapp.ui.main_view
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.rememberNavController
 import com.core.navigtion.AppNavigator
 import com.core.navigtion.navigator.NavComposable
+import com.core.themes.LocalAdaptiveSizeProvider
+import com.core.themes.WindowsSize
 import com.feature.recipe.list.ui.SearchBarContainer
 import com.feature.recipe.list.viewmodel.SearchViewModel
-import com.recipe.app.navigation.intent.*
+import com.recipe.app.navigation.intent.HomeViewIntent
+import com.recipe.app.navigation.intent.MainViewIntent
+import com.recipe.app.navigation.intent.RecipeVideoListIntent
+import com.recipe.app.navigation.intent.SavedRecipeIntent
+import com.recipe.app.navigation.intent.SettingsViewIntent
 import com.recipe.app.navigation.provider.MainViewNavigator
 import com.recipe.app.navigation.provider.ParentNavHostController
 
@@ -37,6 +44,19 @@ fun NavGraphBuilder.MainScreen() {
     }
 }
 
+val mainContentNavigation = listOf(
+    BottomBarItems(HomeViewIntent.getScreenName(), "Home"),
+    BottomBarItems(
+        SavedRecipeIntent.getScreenName(),
+        "Saved Recipes"
+    ),
+    BottomBarItems(
+        RecipeVideoListIntent.getScreenName(),
+        "Videos"
+    ),
+    BottomBarItems(SettingsViewIntent.getScreenName(), "Settings")
+)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @ExperimentalFoundationApi
 @ExperimentalComposeUiApi
@@ -46,22 +66,22 @@ fun AppContent() {
     val topLevelNavigator = ParentNavHostController.current
     val searchViewModel: SearchViewModel = hiltViewModel()
     Scaffold(
-        bottomBar = {
-            BottomBar(
-                items = listOf(
-                    BottomBarItems(HomeViewIntent.getScreenName(), "Home"),
-                    BottomBarItems(
-                        SavedRecipeIntent.getScreenName(),
-                        "Saved Recipes"
-                    ),
-                    BottomBarItems(
-                        RecipeVideoListIntent.getScreenName(),
-                        "Videos"
-                    ),
-                    BottomBarItems(SettingsViewIntent.getScreenName(), "Settings")
-                )
-            )
-        },
+//        bottomBar = {
+//            BottomBar(
+//                items = listOf(
+//                    BottomBarItems(HomeViewIntent.getScreenName(), "Home"),
+//                    BottomBarItems(
+//                        SavedRecipeIntent.getScreenName(),
+//                        "Saved Recipes"
+//                    ),
+//                    BottomBarItems(
+//                        RecipeVideoListIntent.getScreenName(),
+//                        "Videos"
+//                    ),
+//                    BottomBarItems(SettingsViewIntent.getScreenName(), "Settings")
+//                )
+//            )
+//        },
         topBar = {
             if (topLevelNavigator.getCurrentRoute()
                 != SettingsViewIntent.getScreenName()
@@ -70,7 +90,28 @@ fun AppContent() {
         },
     ) {
         Box(modifier = Modifier.padding(it)) {
-                NavigationView(searchViewModel)
+            RecipeNavHost(searchViewModel)
         }
     }
 }
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
+@Composable
+fun RecipeNavHost(searchViewModel: SearchViewModel) {
+    val adaptiveSizes = LocalAdaptiveSizeProvider.current
+    Row(modifier = Modifier.fillMaxHeight()) {
+        if (adaptiveSizes.windowSize != WindowsSize.COMPACT)
+            NavRail(items = mainContentNavigation)
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Box(modifier = Modifier.weight(1f)) {
+                NavigationView(searchViewModel = searchViewModel)
+            }
+            if (adaptiveSizes.windowSize == WindowsSize.COMPACT)
+                BottomBar(items = mainContentNavigation)
+        }
+    }
+}
+
+
